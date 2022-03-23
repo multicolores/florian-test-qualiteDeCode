df910d36b0d036b99acd4e742d4ea254e5e37a55
multicolores <florian.tellier02@gmail.com>
2022-03-23 15:19:01 +0100

    premier essaie de push du code normalisé

--- src/UPJV/Validator/EstValide.php
+++ src/UPJV/Validator/EstValide.php
@@ -1,14 +1,35 @@
 <?php
 
 namespace UPJV\Validator;
+/**
+Ceci est la doc de la classe
 
+@category Test_De_Category
+@package  Test_De_Category
+@author   Display Name <username@example.com>
+@license  test de category
+@link     test de category
+ */
 class EstValide implements ValidatorInterface
 {
+    /**
+     * Fonction build.
+     * 
+     * @param string $param ceci est le commentaire
+     * 
+     * @return $this
+     */
     public function build(array $param): object
     {
         return $this;
     }
-
+    /**
+     * Fonction check.
+     * 
+     * @param string $input ceci est le commentaire
+     * 
+     * @return bool
+     */
     public function check($input): bool
     {
         return true;
